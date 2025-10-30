f2c3d7bd8592bd51b6c1c1fe5e9deb60ccc47bdc
Hyeongsu Kim <53201140+giruda-gear@users.noreply.github.com>
2025-10-30 16:27:59 +0100

    add computing attention weights

--- ch03/self_attention1.py
+++ ch03/self_attention1.py
@@ -18,14 +18,16 @@ attn_scores_2 = torch.empty(inputs.shape[0])
 for i, x_i in enumerate(inputs):
     attn_scores_2[i] = torch.dot(x_i, query)
 # print("attention score:", attn_scores_2)
+# [(your <- journey), (journey <- journey), (starts <- journey), ...]
 
 # understanding dot product
+# your <- journey
 # [0.43, 0.15, 0.89] * [0.55
 #                       0.87
-#                       0.64]
-# res = 0
-# for idx, element in enumerate(inputs[0]):
-#     res += inputs[0][idx] * query[idx]
+#                       0.66]
+res = 0
+for idx, element in enumerate(inputs[0]):
+    res += inputs[0][idx] * query[idx]
 # print(res) # 0.9544
 
 # normalized attention scores
@@ -43,11 +45,13 @@ attn_weights_2_naive = softmax_naive(attn_scores_2)
 # print("sum:", attn_weights_2_naive.sum())
 
 attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
-print("attention weights(softmax):", attn_weights_2)
-print("sum:", attn_weights_2.sum())
+# print("attention weights(softmax):", attn_weights_2)
+# print("sum:", attn_weights_2.sum())
 
-
+print(attn_weights_2)
 context_vec_2 = torch.zeros(query.shape) # tensor([0., 0., 0.])
 for i, x_i in enumerate(inputs):
     context_vec_2 += attn_weights_2[i] * x_i
+    print(attn_weights_2[i],"*" ,x_i)
 print("context vector:", context_vec_2)
+
