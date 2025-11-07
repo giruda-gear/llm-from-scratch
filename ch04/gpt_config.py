3d75d378def1e2abde301bf3b407be9903a923c5
Hyeongsu Kim <53201140+giruda-gear@users.noreply.github.com>
2025-11-07 16:17:03 +0100

    implement dummy gpt model

---
+++ ch04/gpt_config.py
@@ -0,0 +1,9 @@
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # vocabulary size
+    "context_length": 1024, # context length
+    "emb_dim": 768,         # embedding dimension
+    "n_heads": 12,          # number of attention heads
+    "n_layers": 12,         # number of layers
+    "drop_rate": 0.1,       # dropout rate
+    "qkv_bias": False       # query-key-value bias
+}
---
+++ ch04/main01.py
@@ -0,0 +1,21 @@
+import torch
+import tiktoken
+
+from ch04.dummy_gpt_model import DummyGPTModel
+from ch04.gpt_config import GPT_CONFIG_124M
+
+tokenizer = tiktoken.get_encoding("gpt2")
+batch = []
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+logits = model(batch)
+print("output shape:", logits.shape)
+print(logits)
